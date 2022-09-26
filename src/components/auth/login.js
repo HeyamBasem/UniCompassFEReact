27474c2d09f9c925c6c3e005272a3c8f6bf73966
Heyam Almahmoud <50664699+HeyamBasem@users.noreply.github.com>
2022-09-26 21:21:14 +0300

    add-submit-handlers

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react';
 import pngwing from '../util/pngwing.com.png'
 import cForm from '../util/cForm2.png';  
+// import axios from "axios";
 
-export default class Login extends Component {
-  render() {
+
+
+export default function Login() {
+  const [data,setData] = useState({
+    email:"",
+    password:""
+  })
+  
+  const {email,password} = data;
+  
+  const changeHandler = e => {
+    setData({...data,[e.target.name]:[e.target.value]});
+  }
+  
+  const submitHandler = e => {
+    console.log('test');
+    e.preventDefault();
+    console.log(data);
+  }
     return (
       <div className='rootAuthSignUp'>
         <div className='imageContainerAuthPage'>
           <img src={pngwing} alt="pngwing.com.png" className="signupImg" />
         </div>
         <div className="auth-inner-signin">
-        <form align='left'>
+        <form align='left' onSubmit={submitHandler}>
           <div className="formHeader">
             <img src={cForm} alt="cForm.png" className="imgForForm" />
               <h3 align="right"><i> Sign In <br/><br/>Uni Compass</i></h3>
@@ -18,6 +36,9 @@ export default class Login extends Component {
           <div className="mb-3">
             <label>Email address</label>
             <input
+              name="email" 
+              value={email}
+              onChange={changeHandler}
               type="email"
               className="form-control"
               placeholder="Enter email"
@@ -26,6 +47,9 @@ export default class Login extends Component {
           <div className="mb-3">
             <label>Password</label>
             <input
+              name="password" 
+              value={password} 
+              onChange={changeHandler}
               type="password"
               className="form-control"
               placeholder="Enter password"
@@ -44,7 +68,7 @@ export default class Login extends Component {
             </div>
           </div>
           <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" name="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
@@ -55,5 +79,5 @@ export default class Login extends Component {
         </div>
       </div>
     )
-  }
+  
 }
